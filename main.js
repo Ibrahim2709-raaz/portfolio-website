f3ece4c2f2199842f4627135f6b4f022f935f520
Ibrahim Salman <ibrahim.salman2700@gmail.com>
2025-09-03 10:37:35 -0700

    about section done
    about section done

--- main.js
+++ main.js
@@ -5,3 +5,20 @@ var Typed= new Typed(".text", {
     backDelay: 1000, 
     loop: true
 });
+
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname)
+{
+    for(tablink of tablinks)
+    {
+        tablink.classList.remove("active-link");
+    }
+    for(tabcontent of tabcontents)
+    {
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
