cac94fa8970d793aa64cfea7bfec2e40926cb8a2
Ibrahim-Salman27 <bscs24145@itu.edu.pk>
2025-09-05 08:56:39 -0700

    changes

--- main.js
+++ main.js
@@ -1,25 +1,8 @@
-var Typed= new Typed(".text", {
-    strings:["Developer" , "Pianist" , "Dreamer"], 
-    typeSpeed: 100, 
-    backSpeed: 100, 
-    backDelay: 1000, 
-    loop: true
+// Typed.js effect
+var typed = new Typed(".text", {
+  strings: ["Developer", "Pianist", "Dreamer"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true
 });
-
-var tablinks = document.getElementsByClassName("tab-links");
-var tabcontents = document.getElementsByClassName("tab-contents");
-
-function opentab(tabname)
-{
-    for(tablink of tablinks)
-    {
-        tablink.classList.remove("active-link");
-    }
-    for(tabcontent of tabcontents)
-    {
-        tabcontent.classList.remove("active-tab");
-    }
-    event.currentTarget.classList.add("active-link");
-    document.getElementById(tabname).classList.add("active-tab");
-}
-
---
+++ prototype.js
@@ -0,0 +1,21 @@
+var Typed = new Typed(".text", {
+     strings: ["Developer", "Pianist", "Dreamer"], 
+     typeSpeed: 100, 
+     backSpeed: 100, 
+     backDelay: 1000, 
+     loop: true }); 
+var tablinks = document.getElementsByClassName("tab-links"); 
+var tabcontents = document.getElementsByClassName("tab-contents"); 
+function opentab(tabname) 
+{   for (tablink of tablinks) 
+    {
+        tablink.classList.remove("active-link"); 
+       
+    } 
+    for (tabcontent of tabcontents) 
+    { 
+        tabcontent.classList.remove("active-tab"); 
+    } 
+    event.currentTarget.classList.add("active-link"); 
+    document.getElementById(tabname).classList.add("active-tab"); 
+}
